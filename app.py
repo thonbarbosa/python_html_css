57c1b9dd516b2959d99c99d78b22741d4700797a
thonbarbosa <thonbarbosa.ti@gmail.com>
2022-06-01 01:05:07 +0000

    meu blog

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ def depois_requisicao(e):
 
 @app.route("/")
 def exibir_entradas():
-    sql = "SELECT titulo, texto, criado_em FROM entradas ORDER BY id DESC "
+    sql = "SELECT titulo, texto, criado_em FROM entradas ORDER BY id DESC;"
     cur = g.bd.execute(sql)
     entradas = []
     for titulo, texto, criado_em in cur.fetchall():
-     entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
+        entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
     return render_template("exibir_entradas.html", entradas=entradas)
 
 @app.route('/inserir', methods=["POST"])
@@ -32,22 +32,22 @@ def inserir_entradas():
     if not session.get('logado'):
         abort(401)
     titulo = request.form['titulo']
-    texto = request.form['texto'] 
+    texto = request.form['texto']
     sql = "INSERT INTO entradas(titulo, texto) VALUES (?, ?)"
-    g.bd.execute(sql, [titulo, texto])
-    g.bd.commit()
-    flash("Nova Entrada gravada com Sucesso!")
-    return redirect(url_for('inserir_entradas'))
+    g.bd.execute(sql, [titulo, texto]) 
+    g.bd.commit()  
+    flash("Nova entrada gravada com Sucesso!")
+    return redirect(url_for("exibir_entradas"))
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    erro = None
     if request.method == "POST":
-       if request.form['username'] == "admin" and request.form['password'] == "admin":
-         session['logado'] = True
-       flash("login efetuado!")
-    return redirect(url_for('exibir_entradas'))
-    erro = "usuario ou Senha invalidos"
-
+        if request.form['username'] == "admin" and request.form['password'] == "admin":
+            session['logado'] = True
+            flash("Login efetuado!")
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou Senha inválidos"
     return render_template("login.html", erro=erro)
 
 @app.route("/logout")
@@ -55,7 +55,6 @@ def logout():
     session.pop('logado', None)
     flash("Logout Efetuado com Sucesso!")
     return redirect(url_for("exibir_entradas"))
-
 
 
 
